960586cb70b11ee5ef062a1fb23e73ee00359cf6
snekw <dev.snekw@gmail.com>
2020-09-04 13:27:32 +0300

    LCJS 2.0.0

--- src/index.js
+++ src/index.js
@@ -12,56 +12,60 @@ const {
     SliceLabelFormatters,
     SliceSorters,
     LUT,
-    ColorRGBA
+    ColorRGBA,
+    Themes
 } = lcjs
 
 // Create a new color-value lookup table
-const lut = new LUT( {
+const lut = new LUT({
     steps: [
-        { value: 20, color: ColorRGBA( 255, 0, 24 ) },
-        { value: 40, color: ColorRGBA( 255, 165, 44 ) },
-        { value: 60, color: ColorRGBA( 255, 255, 65 ) },
-        { value: 80, color: ColorRGBA( 0, 128, 24 ) },
-        { value: 100, color: ColorRGBA( 0, 0, 249 ) },
-        { value: 120, color: ColorRGBA( 134, 0, 125 ) }
+        { value: 20, color: ColorRGBA(255, 0, 24) },
+        { value: 40, color: ColorRGBA(255, 165, 44) },
+        { value: 60, color: ColorRGBA(255, 255, 65) },
+        { value: 80, color: ColorRGBA(0, 128, 24) },
+        { value: 100, color: ColorRGBA(0, 0, 249) },
+        { value: 120, color: ColorRGBA(134, 0, 125) }
     ],
     interpolate: true
-} )
+})
 
 // Create a Pie Chart
 const pie = lightningChart()
-    .Pie( { type: PieChartTypes.LabelsOnSides } )
-    .setTitle( 'Pie Chart' )
-    .setAnimationsEnabled( true )
-    .setMultipleSliceExplosion( true )
-    .setLabelFormatter( SliceLabelFormatters.NamePlusValue )
-    .setSliceSorter( SliceSorters.None )
+    .Pie({
+        // theme: Themes.dark 
+        type: PieChartTypes.LabelsOnSides
+    })
+    .setTitle('Pie Chart')
+    .setAnimationsEnabled(true)
+    .setMultipleSliceExplosion(true)
+    .setLabelFormatter(SliceLabelFormatters.NamePlusValue)
+    .setSliceSorter(SliceSorters.None)
     // Attach lookup table
-    .setLUT( lut )
-    .setLabelConnectorGap( 10 )
+    .setLUT(lut)
+    .setLabelConnectorGap(10)
 
 const data = []
 
-for ( let i = 1; i <= 10; i++ )
-    data.push( { name: `Slice #${i}`, value: 1 + Math.random() * 120 } )
+for (let i = 1; i <= 10; i++)
+    data.push({ name: `Slice #${i}`, value: 1 + Math.random() * 120 })
 
 // Create Slices
-const slices = data.map( ( item ) => pie.addSlice( item.name, item.value ) )
+const slices = data.map((item) => pie.addSlice(item.name, item.value))
 
 // Add LegendBox
 pie
-    .addLegendBox( LegendBoxBuilders.VerticalLegendBox )
-    .setPosition( { x: 0, y: 0 } )
-    .setOrigin( { x: -1, y: -1 } )
-    .setMargin( { bottom: 5, left: 5 } )
-    .add( pie )
+    .addLegendBox(LegendBoxBuilders.VerticalLegendBox)
+    .setPosition({ x: 0, y: 0 })
+    .setOrigin({ x: -1, y: -1 })
+    .setMargin({ bottom: 5, left: 5 })
+    .add(pie)
 
 // 2 seconds after the example has loaded, change the values of each slice.
-setTimeout( () => {
-    slices.forEach( ( slice ) => slice.setValue( 1 + Math.random() * 120 ) )
+setTimeout(() => {
+    slices.forEach((slice) => slice.setValue(1 + Math.random() * 120))
 }, 2000)
 
 // Change value of every slice regularly
-setInterval( () => {
-    slices.forEach( ( slice ) => slice.setValue( 1 + Math.random() * 120 ) )
-}, 5000 )
+setInterval(() => {
+    slices.forEach((slice) => slice.setValue(1 + Math.random() * 120))
+}, 5000)
